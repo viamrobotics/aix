7424b1bd309878a00f7121c9fa6eadf869f1e454
James Otting <james@viam.com>
2021-12-15 21:13:10 -0800

    Improve updates to maintain file ownership/permissions and implement target switching

--- aix.go
+++ aix.go
@@ -1,75 +1,141 @@
 package main
 
 import (
+	"bytes"
 	"crypto/sha1"
 	"debug/elf"
 	"encoding/hex"
-	"flag"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"os"
 	"strings"
+	"syscall"
 
 	"golang.org/x/sys/unix"
 
 	"github.com/AppImageCrafters/libzsync-go"
+	"github.com/jessevdk/go-flags"
+	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
-	var update bool
-	flag.BoolVar(&update, "aix-update", false, "Update and exit")
+	// Set automatically inside AppImage runtimes
+	appDir := os.Getenv("APPDIR")
 
-	var autoUpdate bool
-	flag.BoolVar(&autoUpdate, "aix-auto-update", false, "Auto update and continue running")
-
-	var url string
-	flag.StringVar(&url, "aix-update-url", "", "Force ZSync (source) URL")
-
-	var target string
-	flag.StringVar(&target, "aix-update-file", "", "Force local (destination) file path")
-
-	flag.Parse()
-	args := flag.Args()
-	if len(args) == 0 && target == "" {
-		panic("Target File path expected")
+	var opts struct {
+		Update     bool   `long:"aix-update" description:"Update and exit"`
+		AutoUpdate bool   `long:"aix-auto-update" description:"Update and run main app from new version"`
+		UpdateURL  string `long:"aix-update-url" description:"Force ZSync (source) URL"`
+		UpdateFile string `long:"aix-update-file" description:"Force local AppImage (destination) file path for update" env:"APPIMG"`
+		Target     string `long:"aix-target" description:"Run internal tool/script (instead of main application)" env:"AIX_TARGET"`
+		Install    bool   `long:"aix-install" description:"Shortcut for --aix-target=aix.d/install"`
+		Help       bool   `long:"aix-help" description:"Show this help message"`
 	}
 
-	if target == "" {
-		target = args[0]
-	}
+	p := flags.NewParser(&opts, flags.IgnoreUnknown)
+	p.Usage = "(AppImage eXtender) is a wrapper layer for use in AppImages.\n" +
+		"  It allows self-updates and the running of non-default targets (such as install scripts) from within a single AppImage.\n" +
+		"  Note that the options/arguments in this help message ONLY apply to the wrapper layer.\n" +
+		"  All other options are passed through to the target application."
 
-	if url == "" {
-		var err error
-		url, err = GetURLFromImage(target)
-		if err != nil {
-			panic(err)
-		}
-	}
-
-	shaSum, err := GetSHA1(target)
+	args, err := p.Parse()
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Update: ", update)
-	fmt.Println("AutoUpdate: ", autoUpdate)
-	fmt.Println("URL: ", url)
-	fmt.Println("File: ", target)
-	fmt.Println("SHA1: ", shaSum)
+	if opts.Help {
+		var b bytes.Buffer
+		p.WriteHelp(&b)
+		fmt.Println(b.String())
+		os.Exit(1)
+	}
 
-	if update {
-		updated, err := doUpdate(target, url)
+	if opts.AutoUpdate {
+		opts.Update = true
+	}
+
+	if opts.Install {
+		if opts.Update {
+			fmt.Println("Can't update and install at the same time. Please update first.")
+			os.Exit(1)
+		}
+		_, err := os.Stat(appDir + "/aix.d/install")
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Println("No install target executable (aix.d/install) found!")
+			os.Exit(1)
+		}
+		opts.Target = "aix.d/install"
+	}
+
+	if appDir != "" {
+		opts.Target = appDir + "/" + strings.TrimPrefix(opts.Target, "/")
+	}
+
+	if opts.Update {
+		if opts.UpdateFile == "" {
+			panic("No AppImage file to update!")
+		}
+
+		if opts.UpdateURL == "" {
+			var err error
+			opts.UpdateURL, err = GetURLFromImage(opts.UpdateFile)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		shaSum, err := GetSHA1(opts.UpdateFile)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Update: ", opts.Update)
+		fmt.Println("AutoUpdate: ", opts.AutoUpdate)
+		fmt.Println("URL: ", opts.UpdateURL)
+		fmt.Println("File: ", opts.UpdateFile)
+		fmt.Println("SHA1: ", shaSum)
+
+		updated, err := doUpdate(opts.UpdateFile, opts.UpdateURL)
 		if err != nil {
 			fmt.Println("Error during update: ", err)
 			os.Exit(1)
 		}
 		if updated {
-			fmt.Println("Updated!")
+			fmt.Println("Successfully updated.")
+			if opts.AutoUpdate {
+				// Clean environment
+				os.Unsetenv("AIX_TARGET")
+
+				// Exec the newly updated AppImage
+				opts.Target = opts.UpdateFile
+			}
 		} else {
 			fmt.Println("No update needed.")
 		}
 	}
+
+	// Special env set within AppImage runtimes
+	selfName := os.Getenv("ARGV0")
+	if selfName == "" {
+		// Fallback to normal ARGV0
+		selfName = os.Args[0]
+	}
+
+	err = unix.Access(opts.Target, unix.X_OK)
+	if err != nil {
+		panic(fmt.Errorf("Target (%s) isn't executable", opts.Target))
+	}
+
+	env := os.Environ()
+
+	newArgs := []string{selfName}
+	newArgs = append(newArgs, args...)
+
+	// We are completely replacing ourselves with the new app
+	// This should never return, so we panic if it does
+	panic(syscall.Exec(opts.Target, newArgs, env))
 }
 
 func GetSHA1(filePath string) (string, error) {
@@ -135,10 +201,12 @@ func doUpdate(filePath string, url string) (bool, error) {
 	}
 	defer os.Remove(tmpFile.Name())
 
-	err = zs.Sync(filePath, tmpFile)
+	bar := progressbar.DefaultBytes(zs.RemoteFileSize, "Updating")
+	err = zs.Sync(filePath, &progressMultiWriter{bar, tmpFile})
 	if err != nil {
 		return false, err
 	}
+	bar.Finish()
 
 	shaSum, err = GetSHA1(filePath)
 	if err != nil {
@@ -149,10 +217,52 @@ func doUpdate(filePath string, url string) (bool, error) {
 		return false, fmt.Errorf("Checksum mismatch after update. Got: %s, Expected: %s", shaSum, zs.RemoteFileSHA1)
 	}
 
+	// So easy to get permissions
+	fileInfo, _ := os.Stat(filePath)
+	mode := fileInfo.Mode()
+
+	// Then there's the two lines below... like demonic waterfowl, they slowly nibble away my sanity
+	uid := int(fileInfo.Sys().(*syscall.Stat_t).Uid)
+	gid := int(fileInfo.Sys().(*syscall.Stat_t).Gid)
+
 	err = os.Rename(tmpFile.Name(), filePath)
 	if err != nil {
 		return false, err
 	}
 
+	os.Chown(filePath, uid, gid)
+	if err != nil {
+		return false, err
+	}
+
+	err = os.Chmod(filePath, mode)
+	if err != nil {
+		return false, err
+	}
+
 	return true, nil
+}
+
+// Simple io.WriteSeeker type for progress bar
+type progressMultiWriter struct {
+	progressBar io.Writer
+	outFile     io.WriteSeeker
+}
+
+func (pmw *progressMultiWriter) Write(p []byte) (n int, err error) {
+	for _, w := range []io.Writer{pmw.progressBar, pmw.outFile} {
+		n, err = w.Write(p)
+		if err != nil {
+			return
+		}
+		if n != len(p) {
+			err = io.ErrShortWrite
+			return
+		}
+	}
+	return len(p), nil
+}
+
+func (pmw *progressMultiWriter) Seek(offset int64, whence int) (int64, error) {
+	return pmw.outFile.Seek(offset, whence)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.17
 
 require (
 	github.com/AppImageCrafters/libzsync-go v0.0.0-00010101000000-000000000000
-	golang.org/x/sys v0.0.0-20211214234402-4825e8c3871d
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/schollz/progressbar/v3 v3.8.3
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e
 )
 
 require (
@@ -12,7 +14,11 @@ require (
 	github.com/glycerine/rbuf v0.0.0-20190314090850-75b78581bebe // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20211111143520-d0d5ecc1a356 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 )
 
 replace github.com/AppImageCrafters/libzsync-go => ../libzsync-go
